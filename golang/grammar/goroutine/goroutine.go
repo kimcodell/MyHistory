3a9fc66b536a72b1fe630bf0379ac10efa166475
minhyuk02 <minhyuk@indinity.co>
2022-07-27 20:01:40 +0900

    go study

--- golang/grammar/goroutine/goroutine.go
+++ golang/grammar/goroutine/goroutine.go
@@ -144,5 +144,30 @@ func ChanWaitingTest() {
 // select문 활용
 // select문을 활용하면 여러 채널을 기다릴 수 있음.
 func SelectTest() {
+	var wg sync.WaitGroup
+	ch := make(chan int)
+	quit := make(chan bool) // ❶ 종료 채널
 
+	wg.Add(1)
+	go square1(&wg, ch, quit)
+
+	for i := 0; i < 10; i++ {
+		ch <- i * 2
+	}
+
+	quit <- true
+	wg.Wait()
+}
+
+func square1(wg *sync.WaitGroup, ch chan int, quit chan bool) {
+	for {
+		select { // ❷ ch와 quit 양쪽을 모두 기다린다.
+		case n := <-ch:
+			fmt.Printf("Square: %d\n", n*n)
+			time.Sleep(time.Second)
+		case <-quit:
+			wg.Done()
+			return
+		}
+	}
 }
