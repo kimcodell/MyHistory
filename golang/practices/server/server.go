3512ea357f0c29364d48c3a32522f6dcd26a3425
kimcodell <kimminhyuk0719@gmail.com>
2022-08-02 00:53:40 +0900

    update

--- golang/practices/server/server.go
+++ golang/practices/server/server.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		fmt.Fprint(res, "Hello World")
+	})
+
+	fmt.Println("3000번 포트에서 서버 실행 중...")
+	http.ListenAndServe(":3000", nil)
+}
